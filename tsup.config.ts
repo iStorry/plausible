64a5f5ace27e28a885f2daebc44c390e303868e4
Jatinjit Singh <jatinx.lol@gmail.com>
2024-08-08 12:52:41 +0900

    - Fixed common-js package issue.

--- tsup.config.ts
+++ tsup.config.ts
@@ -7,20 +7,19 @@ export default defineConfig((options: Options) => ({
   treeshake: true,
   splitting: true,
   target: "es2020",
-  format: ["cjs", "esm"], // generate cjs and esm files
+  format: ["cjs", "esm"], // Ensure both cjs and esm formats are generated
   entry: ["./src/*.{ts,tsx}", "./src/@types/**/*.{ts,tsx}"],
-  skipNodeModulesBundle: true, // Skips building dependencies for node modules
+  skipNodeModulesBundle: true,
   minify: !options.watch && env === "production",
   bundle: false,
-  clean: true, // clean up the dist folder
-  dts: true, // generate dts file for main module
-  sourcemap: env === "production", // source map is only available in prod
+  clean: true,
+  dts: true,
+  sourcemap: env === "production",
   outDir: "dist",
   tsconfig: path.resolve(__dirname, "./tsconfig.json"),
-  esbuildOptions(options, context) {
+  esbuildOptions(options) {
     options.outbase = "src";
   },
   external: ["axios"],
   ...options,
-  // button: { js: '"use client";' },
 }));
